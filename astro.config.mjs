447c9661c5db796547a67eedbc339762e7274850
Zhukov Dmitry <zhukontime@gmail.com>
2025-04-30 03:43:31 +0400

    fix: update hrefs and config, set up favicon import

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://career200.github.io",
+  base: "/super-octo-chainsaw/",
 });
