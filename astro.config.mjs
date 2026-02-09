ad9c4de657b22e39fde5676d6be2f1f941daa333
Dmitriy Zhukov <zhukontime@gmail.com>
2026-02-09 17:28:35 +0400

    fix: astro ts magic fix, add todo to preact devtools

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ export const baseString = "/super-octo-chainsaw/";
 export default defineConfig({
   site: "https://career200.github.io",
   base: baseString,
+  // TODO: make devtools conditional after testing (e.g. process.env.NODE_ENV !== 'production')
   integrations: [preact({ devtools: true })],
 });
